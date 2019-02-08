be6ee99e979d0843148eac4b1b128da0b8211772
Dario Oropeza <itsdario1@gmail.com>
2019-02-08 23:36:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,16 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts total
 end
 
 def prompt_user
